0ec946c7d3e42f82e6119c27f83eb5ebf5a92392
sow4063 <sow4063@gmail.com>
2017-02-21 14:27:43 +0900

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
   	filename: 'bundle.js',
   	path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+    ]
   }
 };
