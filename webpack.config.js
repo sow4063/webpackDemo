63346eaea31962be84ae9d4f82192907139eb5af
sow4063 <sow4063@gmail.com>
2017-02-21 14:55:12 +0900

    add webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
-var path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+// const path = require('path');
+
+// module.exports = {
+//   entry: './app/index.js',
+//   output: {
+//   	filename: 'bundle.js',
+//   	path: path.resolve(__dirname, 'dist')
+//   },
+//   module: {
+//     rules: [
+//       {test: /\.(js|jsx)$/, use: 'babel-loader'}
+//     ]
+//   }
+// };
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: './app/index.js',
   output: {
-  	filename: 'bundle.js',
-  	path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {test: /\.(js|jsx)$/, use: 'babel-loader'}
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
